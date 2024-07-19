aab2938153b74f8da72fcad1a79a02e7f55b9195
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-07-19 21:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+document.querySelector("button").addEventListener("click",()=>{
+	let name=document.querySelector("#name").value;
+	let year=document.querySelector("#year").value;
+	let update=`https://localhost:8080/?name=${name}&year=${year}`;
+	document.querySelector("h3").innerHTML=update;
+})
