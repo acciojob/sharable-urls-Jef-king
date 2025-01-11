27eca40cd411213cc376295074f27ddd0a01204c
Jef-king <147884623+Jef-king@users.noreply.github.com>
2025-01-11 17:28:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // your code here
 document.querySelector("button").addEventListener("click",()=>{
-	let name=document.querySelector("#name").value;
-	let year=document.querySelector("#year").value;
-	let update=`https://localhost:8080/?name=${name}&year=${year}`;
+	let name=document.querySelector("#name").value.trim();
+	let year=document.querySelector("#year").value.trim();
+	let update='';
+	if(name!=''&&year!=''){
+	update=`https://localhost:8080/?name=${name}&year=${year}`;
+	}else if(name!=''){
+			update=`https://localhost:8080/?name=${name}`;
+	}else if(update!=''){
+			update=`https://localhost:8080/?year=${year}`;
+	}else {
+			update=`https://localhost:8080/`;
+
+	}
 	document.querySelector("#url").innerHTML=update;
 })
